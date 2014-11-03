db5d5cfe67274701c3735ace0cab22c2be927152
TheEld <dantemm13@gmail.com>
2014-11-03 21:30:12 -0200

    Changes to be committed: 	new file:   js/root.js Adicionando a biblioteca angular ao projeto

---
+++ js/root.js
@@ -0,0 +1,11 @@
+angular.module('root', [])
+function loginController($scope) {
+    $scope.login = "John";
+    $scope.senha = "Doe";
+}
+
+
+/*var root = module('root', [])
+    root.controller("index", ["$scope", function ($scope) {
+        $scope.message = "Hello World!";
+    }]);*/
